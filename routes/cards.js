2c90b9bbb20692cd8f8e9fd04e512cea890b00de
Tatiana <kriista@mail.ru>
2020-04-20 23:01:27 +0300

    change to pull create

--- routes/cards.js
+++ routes/cards.js
@@ -5,8 +5,8 @@ const {
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
-router.delete('/cards/:cardId', deleteCard);
 router.post('/cards', createCard);
+router.delete('/cards/:cardId', deleteCard);
 router.put('/cards/:cardId/likes', likeCard);
 router.delete('/cards/:cardId/likes', dislikeCard);
 
